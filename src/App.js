e19dd4a6efc48780bcc2297a4e417f5f76f9de54
dalsu0222 <dalsu0222@sju.ac.kr>
2024-01-28 22:05:13 +0900

    feat: Display todos in chronological order

--- src/App.js
+++ src/App.js
@@ -26,7 +26,11 @@ function App() {
       const newToDos = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         todo: doc.data().todo,
+        timestamp: doc.data().timestamp.toDate(), // timestamp를 Date 객체로 변환
       }));
+      // 시간순으로 정렬합니다.
+      newToDos.sort((a, b) => a.timestamp - b.timestamp);
+
       setToDos((prevToDos) => [...prevToDos, ...newToDos]);
     };
 
@@ -44,13 +48,17 @@ function App() {
     if (toDo === "") {
       return;
     }
+
+    const timestamp = new Date(); // 현재 시간을 얻습니다.
+
     const docRef = await addDoc(collection(db, "todos"), {
       todo: toDo || null,
+      timestamp: timestamp, // 생성 시간을 저장합니다.
     });
     // setToDos((currentArray) => [...currentArray, toDo]);
     setToDos((prevToDos) => [
       ...prevToDos,
-      { id: docRef.id, todo: toDo }, // 새로운 todo와 id를 업데이트하기
+      { id: docRef.id, todo: toDo, timestamp: timestamp }, // 새로운 todo와 id, timestamp를 업데이트합니다.
     ]);
     setToDo("");
   };
